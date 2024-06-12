7d94981d407cc26a36435b4cbfa6a63ab8a4d781
asus test <msa-99@outlook.com>
2024-06-12 17:02:43 +0600

    optimized cumulative prefix sum

--- prefixSum_optimized.cpp
+++ prefixSum_optimized.cpp
@@ -6,14 +6,30 @@ int main(){
     int arr[size];
 
     //take input for every index for making array
-    for(int i=0; i<size; i++){ //O(s)
+    for(int i=0; i<size; i++){ //O(s) za 10^5 hote pare
         cin>>arr[i];
     }
     // make cumulative / prefix sum array
     int prefixSum[size];
     prefixSum[0]=arr[0];//initialize first index value
     // assign prefixSum index value, startIndex hobe 1. karon first index manually assign kora hoyeche;
-    
-
+    for(int i=1; i<size; i++){ //O(s) za 10^5 hote pare
+        prefixSum[i]=arr[i]+prefixSum[i-1];
+    }
+    // for(int i=0; i<size; i++){ O(s)
+    //     cout<<prefixSum[i]<<endl;
+    // }
+   
+   while(query--){ //O(q) za 10^5 hote pare
+      int startPos, endPos;
+      cin>>startPos>>endPos;
+      int startIndex=startPos-1;
+      int endIndex=endPos-1;
+      // find result from prefixSum array
+      int value=0;
+      if(startIndex==0) value=prefixSum[endIndex];
+      else value=prefixSum[endIndex]-prefixSum[startIndex-1];
+      cout<<value<<endl;
+   } 
     return 0;
 }
